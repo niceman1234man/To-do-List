d98475c024f90bfae67db5ab6bef4f4ebb355b50
niceman1234man <yihunietarekegn18@gmail.com>
2024-12-26 21:35:52 -0800

    server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express'
+import { connectDb } from './db/index.js';
+import { router } from './router/index.js';
 const app=express();
+connectDb();
+app.use('/task',router);
 const port=5000;
-
 app.listen(port,()=>console.log("server running on ",port))
