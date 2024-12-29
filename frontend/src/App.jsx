ecf90bd53f088e1745f90d54694ff3055a63bf46
niceman1234man <yihunietarekegn18@gmail.com>
2024-12-29 15:33:31 -0800

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,15 @@ import AddToDoList from './components/AddToDoList'
 import ListOftodoList from './components/ListOftodoList'
 import { Route,Routes } from 'react-router-dom'
 import UpdateTask from './components/UpdateTask'
-
+import Signin from './components/auth/Signin'
+import Signup from './components/auth/Signup'
 function App() {
   return (
     <>
     <Routes>
     <Route path='/' element={  <AddToDoList/>}/>
+    <Route path='/signin' element={  <Signin/>}/>
+    <Route path='/signup' element={  <Signup/>}/>
     <Route path='/list' element={<ListOftodoList/>}/>
     <Route path='/update-list' element={<UpdateTask/>}/>
     </Routes>
