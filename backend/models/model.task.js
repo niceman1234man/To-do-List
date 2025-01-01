74870147629c2ae745e996d8dc3b2c3a8b9b29f8
niceman1234man <yihunietarekegn18@gmail.com>
2024-12-31 20:55:27 -0800

    task model

--- backend/models/model.task.js
+++ backend/models/model.task.js
@@ -1,17 +1,17 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose from "mongoose";
 const taskSchema=new mongoose.Schema({
-    user:{
-        type:Schema.Types.ObjectId,
-        ref:"User"
+ 
+    title: {
+        type: String,
+        required: true,
+       
     },
-    title:{
-        type:String,
-        required:true,
-    },
-    description:{
-        type:String,
-        required:true,
+    description: {
+        type: String,
+        required: true,
+      
     }
+    
 },{timestamps:true});
 
 export const Task=mongoose.model("Task",taskSchema);
